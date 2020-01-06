e51803db8faad84bbcca1c5e0f449cdf4217d5a6
pawanjayvasnav <pawan.vasnav@highpeaksw.com>
2020-01-06 13:02:01 +0530

    Updates

---
+++ precache-manifest.7bd23ff616822e8bc2d6dfd0be1f1d88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126ad353f8a5b58acdc28b835ec6dac4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc17ecddac4112f1c84e",
+    "url": "/static/css/main.36085579.chunk.css"
+  },
+  {
+    "revision": "cdace96d4c2717e81891",
+    "url": "/static/js/2.a7823e73.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a7823e73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc17ecddac4112f1c84e",
+    "url": "/static/js/main.4f4f70bb.chunk.js"
+  },
+  {
+    "revision": "2d878ed4c316fc01b66d",
+    "url": "/static/js/runtime-main.521d12e3.js"
+  }
+]);
